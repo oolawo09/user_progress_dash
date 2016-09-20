9f48431b7a039575a554dc590db89fe101db5597
Anthony Olawo <anthony.olawo@gmail.com>
2016-09-20 03:34:22 -0400

    can add and list todos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
-   
+    url(r'^addTodo' ,views.addTodo, name='addTodo'), 
     url(r'^todo', views.todo, name='todo'), 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Todo 
+from django.urls import reverse 
 
 # Create your views here.
 def index(request): 
@@ -12,3 +13,8 @@ def todo(request):
     print(todo_list)
     context = {'todo_list' : todo_list}
     return render(request, 'dashboard/todo.html', context)
+
+def addTodo(request):
+    t = Todo(todo_text=request.POST['text'])
+    t.save()
+    return HttpResponseRedirect(reverse('dashboard:todo'))
