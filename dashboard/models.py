06b9fc02d9bc9f11092408eec1c283234415a324
Anthony Olawo <anthony.olawo@gmail.com>
2016-09-20 02:12:00 -0400

    can view todo_list via template now

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=200)
+    done = models.BooleanField(default=False) 
+
+    def __str__(self):
+       return self.todo_text 
+
+    
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo 
 
 # Create your views here.
 def index(request): 
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
+def todo(request):
+    todo_list = Todo.objects.all()
+    print(todo_list)
+    context = {'todo_list' : todo_list}
+    return render(request, 'dashboard/todo.html', context)
